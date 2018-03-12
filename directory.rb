5703e5b902fb892d9151a79b2922198ff2bfbe00
Daniel <daniel.tooke@gmail.com>
2018-03-12 19:52:17 +0000

    Added method for user input to directory

--- directory.rb
+++ directory.rb
@@ -12,6 +12,23 @@ students = [
   {name: "Norman Bates", cohort: :november}
 ]
 
+def input_students
+  puts "Please enter the names of the students!"
+  puts "To finish, just hit return twice."
+  #creates an empty array
+  students = []
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    #add the student hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+# return the array of students, otherwise the array remains a local variable
+  students
+end
+
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
@@ -27,6 +44,7 @@ def print_footer(names)
   puts "Overall, we have #{names.length} great students!"
 end
 
+students = input_students
 print_header
 print(students)
 print_footer(students)
