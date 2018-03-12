e172aaceced431948ede8df05efda53551ed6b8d
Daniel <daniel.tooke@gmail.com>
2018-03-12 18:50:49 +0000

    Added comments to directory.rb

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+# First we list the students
 puts "The students of Villains Academy"
 puts "-------------"
 puts "Dr. Hannibal Lecter"
@@ -11,7 +12,8 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
-
+# Here we print the total number of students
 print "Overall, we have "
+# Print doesn't add an extra line!
 print 11
 puts " great students"
